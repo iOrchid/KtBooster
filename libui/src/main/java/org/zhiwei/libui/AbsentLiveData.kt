cf0e315717eb98ec20f4fd60170e2f278140819e
志威 <1396188796@qq.com>
2020-04-04 13:51:41 +0800

    新增--libui--添加livedata的event

---
+++ libui/src/main/java/org/zhiwei/libui/AbsentLiveData.kt
@@ -0,0 +1,32 @@
+package org.zhiwei.libui
+
+import androidx.lifecycle.LiveData
+
+/**
+ * 作者： 志威  zhiwei.org
+ * 主页： Github: https://github.com/zhiwei1990
+ * 日期： 2020年04月04日 13:50
+ * 签名： 天行健，君子以自强不息；地势坤，君子以厚德载物。
+ *      _              _           _     _   ____  _             _ _
+ *     / \   _ __   __| |_ __ ___ (_) __| | / ___|| |_ _   _  __| (_) ___
+ *    / _ \ | '_ \ / _` | '__/ _ \| |/ _` | \___ \| __| | | |/ _` | |/ _ \
+ *   / ___ \| | | | (_| | | | (_) | | (_| |  ___) | |_| |_| | (_| | | (_) |
+ *  /_/   \_\_| |_|\__,_|_|  \___/|_|\__,_| |____/ \__|\__,_|\__,_|_|\___/  -- 志威 zhiwei.org
+ *
+ * You never know what you can do until you try !
+ * ----------------------------------------------------------------
+ * 用于创建空的LiveData数据
+ */
+
+class AbsentLiveData<T : Any?> private constructor() : LiveData<T>() {
+    init {
+        // use post instead of set since this can be created on any thread
+        postValue(null)
+    }
+
+    companion object {
+        fun <T> create(): LiveData<T> {
+            return AbsentLiveData()
+        }
+    }
+}
---
+++ libui/src/main/java/org/zhiwei/libui/SingleLiveEvent.kt
@@ -0,0 +1,63 @@
+package org.zhiwei.libui
+
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ * 作者： 志威  zhiwei.org
+ * 主页： Github: https://github.com/zhiwei1990
+ * 日期： 2020年04月04日 13:40
+ * 签名： 天行健，君子以自强不息；地势坤，君子以厚德载物。
+ *      _              _           _     _   ____  _             _ _
+ *     / \   _ __   __| |_ __ ___ (_) __| | / ___|| |_ _   _  __| (_) ___
+ *    / _ \ | '_ \ / _` | '__/ _ \| |/ _` | \___ \| __| | | |/ _` | |/ _ \
+ *   / ___ \| | | | (_| | | | (_) | | (_| |  ___) | |_| |_| | (_| | | (_) |
+ *  /_/   \_\_| |_|\__,_|_|  \___/|_|\__,_| |____/ \__|\__,_|\__,_|_|\___/  -- 志威 zhiwei.org
+ *
+ * You never know what you can do until you try !
+ * ----------------------------------------------------------------
+ * 单个的LiveData事件响应类，只有一个接收者能收到信息，作为事件liveEvent使用，可以避免不必要的通知，
+ * 如configChanged的时候引起observer接收事件。
+ * 这里只会在调用call的时候，observer才会收到通知
+ */
+class SingleLiveEvent<T> : MutableLiveData<T>() {
+
+    private val mPending = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+
+        if (hasActiveObservers()) {
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+        }
+
+        // Observe the internal MutableLiveData
+        super.observe(owner, Observer { t ->
+            if (mPending.compareAndSet(true, false)) {
+                observer.onChanged(t)
+            }
+        })
+    }
+
+    @MainThread
+    override fun setValue(t: T?) {
+        mPending.set(true)
+        super.setValue(t)
+    }
+
+    /**
+     * 响应空事件
+     */
+    @MainThread
+    fun call() {
+        value = null
+    }
+
+    companion object {
+        private const val TAG = "SingleLiveEvent"
+    }
+}
