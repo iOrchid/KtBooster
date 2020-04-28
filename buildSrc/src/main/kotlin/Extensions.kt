f97469fd4f9e87b903b2b77c07141ae795036e77
志威 <1396188796@qq.com>
2020-04-28 19:03:33 +0800

    发版--booster--发版0.1.3

--- buildSrc/src/main/kotlin/Extensions.kt
+++ buildSrc/src/main/kotlin/Extensions.kt
@@ -20,8 +20,8 @@ const val compileSdkNum = 29
 const val buildToolsNum = "29.0.3"
 const val minSdkNum = 21
 const val targetSdkNum = 29
-const val libCode = 2
-const val libVersion = "0.1.2.1"//这里的版本号和发布jitpack.io的版本号不一样，那个是github Release页面的
+const val libCode = 3
+const val libVersion = "0.1.3"//这里的版本号和发布jitpack.io的版本号不一样，那个是github Release页面的
 
 //endregion
 
